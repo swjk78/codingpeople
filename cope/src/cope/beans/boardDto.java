4211da010b8f2f66ecaaf4d0aad309ecf143e4f2
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-05-25 11:50:10 +0900

    군더더기 없는 boardDto입니다.

---
+++ cope/src/cope/beans/boardDto.java
@@ -0,0 +1,37 @@
+package cope.beans;
+
+public class boardDto {
+	private int boardNo;
+	private String boardName;
+	private int boardGroup;
+	private int boardSuperno;
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardName() {
+		return boardName;
+	}
+	public void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+	public int getBoardGroup() {
+		return boardGroup;
+	}
+	public void setBoardGroup(int boardGroup) {
+		this.boardGroup = boardGroup;
+	}
+	public int getBoardSuperno() {
+		return boardSuperno;
+	}
+	public void setBoardSuperno(int boardSuperno) {
+		this.boardSuperno = boardSuperno;
+	}
+	public boardDto() {
+		super();
+	}
+	
+	
+}
