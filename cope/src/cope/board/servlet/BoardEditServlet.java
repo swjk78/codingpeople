6eeb867295fd0bb2c829f39c0c1eb5e3db761898
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-05-26 15:56:58 +0900

    board 수정 삭제 서블릿입니다.

--- cope/src/cope/board/servlet/BoardEditServlet.java
+++ cope/src/cope/board/servlet/BoardEditServlet.java
@@ -11,25 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 import cope.beans.BoardDao;
 import cope.beans.BoardDto;
 
-@WebServlet (urlPatterns = "/manage/createBoard.kh")
-public class BoardInsertServlet extends HttpServlet{
+@WebServlet (urlPatterns = "/manage/editBoard.kh")
+public class BoardEditServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
-			
-			//준비 파라미터2개(게시판 이름, 상위게시판을 의미 하는 0)
+			//준비 : 게시판 번호, 바꿀게시판이름
 			req.setCharacterEncoding("UTF-8");
 			BoardDto boardDto = new BoardDto();
+			boardDto.setBoardNo(Integer.parseInt(req.getParameter("boardNo")));
 			boardDto.setBoardName(req.getParameter("boardName"));
-			boardDto.setBoardSuperNo(Integer.parseInt(req.getParameter("boardSuperNo")));
+			
 			
 			//처리
 			BoardDao boardDao = new BoardDao();
-			boardDao.insertBoardSuper(boardDto);
+			boardDao.editBoard(boardDto);
 			
 			//출력
 			resp.sendRedirect("manageBoard.jsp");
-
 			
 		} catch (Exception e) {
 			e.printStackTrace();
