803590c441d3462b6fca28b4f28fa42eb32ffb3d
user <user@DESKTOP-RFAMMBH>
2021-05-28 00:46:22 +0900

    로그인 서블릿과 Dao jdbc

---
+++ cope/src/ClientsoServlet/ClientLogoutServlet.java
@@ -0,0 +1,26 @@
+package ClientsoServlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns="/client/logout.kh")
+public class ClientLogoutServlet extends HttpServlet{
+	@Override
+
+	//로그아웃 기능
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	try {
+		req.getSession().removeAttribute("clientNo");
+		resp.sendRedirect("login.jsp");
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+		resp.sendError(500);
+	}
+	}
+	}
+
