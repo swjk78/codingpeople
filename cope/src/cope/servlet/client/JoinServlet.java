d1e83994b550eea3c81c2a55ac8f2cf4e0666417
swjk78 <swjk78@gmail.com>
2021-05-29 22:24:57 +0900

    회원가입 관련 불필요 코드 제거

--- cope/src/cope/servlet/client/JoinServlet.java
+++ cope/src/cope/servlet/client/JoinServlet.java
@@ -1,6 +1,5 @@
 package cope.servlet.client;
 import java.io.IOException;
-import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import cope.beans.client.JoinDao;
 import cope.beans.client.JoinDto;
-import cope.beans.utils.StringToDate;
 
 @WebServlet(urlPatterns = "/client/join.kh")
 public class JoinServlet extends HttpServlet{
@@ -20,14 +18,11 @@ public class JoinServlet extends HttpServlet{
 
 			req.setCharacterEncoding("UTF-8");
 			JoinDto joinDto = new JoinDto();
-			StringToDate std = new StringToDate();
 			joinDto.setClientId(req.getParameter("clientId"));
 			joinDto.setClientPw(req.getParameter("clientPw"));
 			joinDto.setClientEmail(req.getParameter("clientEmail"));			
 			joinDto.setClientNick(req.getParameter("clientNick"));
 			joinDto.setClientBirthYear(Short.parseShort(req.getParameter("clientBirthYear")));
-			joinDto.setClientGrade(req.getParameter("clientGrade"));
-			joinDto.setClientUnlockDate(std.transformDate(req.getParameter("clientUnlockDate")));
 			
 			JoinDao joinDao = new JoinDao();
 			joinDao.regist(joinDto);
