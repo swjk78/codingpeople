29a7cb08406150986ed21a257fd89b22315b9ca4
swjk78 <swjk78@gmail.com>
2021-05-31 12:19:34 +0900

    블라인드/언블라인드 기능 구현

--- cope/src/cope/beans/post/PostDaoTest.java
+++ cope/src/cope/beans/post/PostDaoTest.java
@@ -8,7 +8,7 @@ import cope.beans.utils.JdbcUtils;
 // 게시글 블라인드 기능 구현을 위한 테스트 PostDao
 // create by JK
 public class PostDaoTest {
-	// 게시글 블라인드 처리 기능
+	// 게시글 블라인드 기능
 	public boolean blindPost(PostDtoTest postDto) throws Exception {
 		Connection con = JdbcUtils.getConnection();
 		
@@ -16,6 +16,20 @@ public class PostDaoTest {
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setInt(1, postDto.getPostNo());
 		ps.setString(2, postDto.getClientGrade());
+		int result = ps.executeUpdate();
+		
+		con.close();
+		
+		return result > 0;
+	}
+	
+	// 게시글 언블라인드 기능
+	public boolean unblindPost(PostDtoTest postDto) throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "update post_list set post_blind = 'F' where post_no = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, postDto.getPostNo());
 		int result = ps.executeUpdate();
 		
 		con.close();
