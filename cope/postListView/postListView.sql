829589753194ea3cc29844eaa35acd4164fcf6b5
jun44 <jun44@DESKTOP-RE79CRN>
2021-06-01 16:23:42 +0900

    게시글목록 수정

--- cope/postListView/postListView.sql
+++ cope/postListView/postListView.sql
@@ -1,5 +1,5 @@
 create or replace view post_list as
-select post_no,post_title,post_client_no,client_no,client_nick, 
+select post_no,post_title,post_client_no,client_no,client_nick,post_contents 
 							post_comments_count,post_view_count,post_like_count,post_date,post_blind
 					from post p left outer join client c on p.post_client_no = c.client_no;
 
