31b90f0a6ba6585275ccd4ccd23d27935cfe68eb
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-06-03 15:30:11 +0900

    post 댓글 구현 완료 서블릿 수정

--- cope/src/cope/servlet/comments/CommentsBlindServlet.java
+++ cope/src/cope/servlet/comments/CommentsBlindServlet.java
@@ -17,19 +17,25 @@ public class CommentsBlindServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			//준비 - 세 개 commentsNo commentsBlind | postNo 
-			
+			System.out.println("오긴함");
 			CommentsDto commentsDto = new CommentsDto();
 			commentsDto.setCommentsNo(Integer.parseInt(req.getParameter("commentsNo")));
 			commentsDto.setCommentsBlind(req.getParameter("commentsBlind"));
 			
-			int PostNo = Integer.parseInt(req.getParameter("postNo"));// 이건 돌아가기 위한 용도
+			System.out.println("파라미터 가져옴");
 			
+			//돌아가기 위한 좌표
+			int boardGroup = Integer.parseInt(req.getParameter("boardGroup"));
+			int PostNo = Integer.parseInt(req.getParameter("postNo"));
+			
+			System.out.println("좌표 가져옴");
 			//처리
 			CommentsDao commentsDao = new CommentsDao();
 			commentsDao.blind(commentsDto);
+			System.out.println("Dao처리함");
 			
 			//출력
-			resp.sendRedirect("post.jsp?postNo="+PostNo);
+			resp.sendRedirect("post.jsp?boardGroup="+ boardGroup +"&postNo="+ PostNo);
 			
 			
 		} catch (Exception e) {
