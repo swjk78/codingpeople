a8c7dc44021258e8f471b90351d8e05ebc6c78b8
KC-HaroldLee <80090106+KC-HaroldLee@users.noreply.github.com>
2021-06-04 17:07:59 +0900

    로그인 상태에서 resetPw.jsp 차단

--- cope/src/cope/filter/IfLoginFilter.java
+++ cope/src/cope/filter/IfLoginFilter.java
@@ -11,10 +11,10 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter (urlPatterns = {"/client/exitSuccess.jsp", "/client/findId.jsp", "/client/findPw.jsp", "/client/login.jsp", "/client/join.jsp", "/client/joinSuccess.jsp"})
+@WebFilter (urlPatterns = {"/client/exitSuccess.jsp", "/client/findId.jsp", "/client/findPw.jsp", "/client/login.jsp", "/client/join.jsp", "/client/joinSuccess.jsp", "/client/resetPw.jsp"})
 
 //로그인 상태에서 들어갈 수 없는 곳
-public class clientLoginStatus implements Filter{
+public class IfLoginFilter implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
