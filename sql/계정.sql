adc728c59a34f51a727bfa041822215bcfc6ccf6
swjk78 <swjk78@gmail.com>
2021-06-05 23:35:16 +0900

    DB 계정 cope로 롤백

--- sql/계정.sql
+++ sql/계정.sql
@@ -1,6 +1,6 @@
 -- 계정 생성 및 권한 부여 구문
-create user kh71 identified by kh71;
-grant connect, resource, create view to kh71;
+create user cope identified by cope;
+grant connect, resource, create view to cope;
 
 -- 계정 삭제 구문
-drop user kh71 cascade;
+drop user cope cascade;
