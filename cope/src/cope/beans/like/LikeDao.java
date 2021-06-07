7fbdf153ef9272203fcc6a4316563e4113763e08
user <user@DESKTOP-RFAMMBH>
2021-06-07 17:53:22 +0900

    로그

--- cope/src/cope/beans/like/LikeDao.java
+++ cope/src/cope/beans/like/LikeDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import cope.beans.client.JdbcUtils;
+import cope.beans.utils.JdbcUtils;
 
 public class LikeDao {
 	
